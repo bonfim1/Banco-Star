fb626735a85cf988fc9cad51cf48d8c4d2e4a5f4
bonfim1 <luzivaniaxlulu@gmail.com>
2025-04-09 15:24:17 -0300

    .
    .

---
+++ PessoaFisica.java
@@ -0,0 +1,26 @@
+
+
+public class PessoaFisica extends Cliente {
+
+    private String cpf;
+
+    public PessoaFisica(String name, String cpf) {
+        super(name);
+        this.cpf = cpf;
+    }
+
+    public PessoaFisica(String id, String name, String cpf) {
+        super(id, name);
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (" + cpf + ")";
+    }
+
+}
---
+++ Rendimento.java
@@ -0,0 +1,8 @@
+
+
+public interface Rendimento {
+
+    public void render();
+    public void setTaxa(double taxa);
+
+}
